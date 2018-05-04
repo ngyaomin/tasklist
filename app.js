85df91060a187ea4c923825096c2ff74b705809f
Yao Min <ngyaominsg@gmail.com>
2018-05-05 00:18:21 +0800

    Function to add task

--- app.js
+++ app.js
@@ -11,3 +11,25 @@ loadEventListeners();
 function loadEventListeners() {
   form.addEventListener('submit', addTask);
 }
+
+// Add Tasks
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('add task');
+  }
+  
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = <i className="fa fa-remove"></i>;
+  li.appendChild(link);
+  
+  taskList.appendChild(li);
+  
+  taskInput.value = '';
+  
+  
+  e.preventDefault();
+}

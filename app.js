b9fed92ad6174f16468edf75e93d7d41aca1d3b4
Yao Min <ngyaominsg@gmail.com>
2018-05-05 21:39:43 +0800

    Remove from local storage

--- app.js
+++ app.js
@@ -87,8 +87,29 @@ function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')) {
     if(confirm('delete for sure?')) {
       e.target.parentElement.parentElement.remove();
+
+      // Remove from localStorage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
+}
+
+// remove from localStorage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  task.forEach(function(task, index){
+    if(taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 //Clear all Tasks
